975e98c92434d5cbe6d7dbb605aaa93cab805ce7
Miro Hrončok <miro@hroncok.cz>
2012-10-03 20:21:53 +0200

    Added pulleys

--- sources/build.sh
+++ sources/build.sh
@@ -11,5 +11,7 @@ $openscad -m make -o ../stl/cornerR.stl -D 'side=-1' corner.scad
 $openscad -m make -o ../stl/side.stl side.scad
 $openscad -m make -o ../stl/side-center.stl side-center.scad
 $openscad -m make -o ../stl/topholder.stl topholder.scad
+$openscad -m make -o ../stl/pulley8.stl  -D 'teeth=8'  pulley.scad
+$openscad -m make -o ../stl/pulley32.stl -D 'teeth=32' pulley.scad
 $openscad -m make -o ../stl/plate1.stl plate1.scad
 $openscad -m make -o ../stl/plate2.stl plate2.scad
