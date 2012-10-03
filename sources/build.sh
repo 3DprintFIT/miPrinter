92d1039259308be72017404274f77d9fcfeafbc5
Miro Hrončok <miro@hroncok.cz>
2012-10-03 20:26:05 +0200

    Removed uselesss rm at the beginning

--- sources/build.sh
+++ sources/build.sh
@@ -4,8 +4,6 @@
 cd "$( dirname "$( type -p $0 )" )"
 openscad="$HOME/Stažené/openscad-2012.06.18/bin/openscad"
 
-rm *.stl -f
-
 $openscad -m make -o ../stl/cornerL.stl -D 'side=+1' corner.scad
 $openscad -m make -o ../stl/cornerR.stl -D 'side=-1' corner.scad
 $openscad -m make -o ../stl/side.stl side.scad
