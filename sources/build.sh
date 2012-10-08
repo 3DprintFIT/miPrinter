92ad9194d5be4ebd2d226af1c5b95e5c87c072b9
Miro Hrončok <miro@hroncok.cz>
2012-10-08 16:07:09 +0200

    Right pulleys

--- sources/build.sh
+++ sources/build.sh
@@ -10,8 +10,6 @@ $openscad -m make -o ../stl/side.stl side.scad
 $openscad -m make -o ../stl/side-center.stl side-center.scad
 $openscad -m make -o ../stl/topholder.stl topholder.scad
 $openscad -m make -o ../stl/motortop.stl motortop.scad
-$openscad -m make -o ../stl/pulley8.stl  -D 'teeth=8'  pulley.scad
-$openscad -m make -o ../stl/pulley32.stl -D 'teeth=32' pulley.scad
+$openscad -m make -o ../stl/pulley8.stl  -D 'teeth=16'  pulley.scad
+$openscad -m make -o ../stl/pulley32.stl -D 'teeth=60' pulley.scad
 $openscad -m make -o ../stl/pulley32.stl pulley32.scad
-#$openscad -m make -o ../stl/plate1.stl plate1.scad
-#$openscad -m make -o ../stl/plate2.stl plate2.scad
